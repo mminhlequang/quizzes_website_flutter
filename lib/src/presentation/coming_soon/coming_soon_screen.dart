7cba8875202a425b0460d44a39b993d9abee755b
Minh Minh <mminh.lequang@gmail.com>
2024-01-02 15:15:20 +0700

    -

--- lib/src/presentation/coming_soon/coming_soon_screen.dart
+++ lib/src/presentation/coming_soon/coming_soon_screen.dart
@@ -1,6 +1,7 @@
 import 'package:_iwu_pack/_iwu_pack.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 import 'package:quizzes/src/utils/utils.dart';
 import 'package:particles_flutter/particles_flutter.dart';
 import 'package:simple_animations/simple_animations.dart';
@@ -16,11 +17,6 @@ class _ComingSoonScreenState extends State<ComingSoonScreen> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      setFriendlyRouteName(
-          title: 'imagineeringwithus - Engineering Your Ideas to Life',
-          url: '/coming_soon');
-    });
   }
 
   @override
@@ -51,75 +47,15 @@ class _ComingSoonScreenState extends State<ComingSoonScreen> {
             );
           },
         ),
-        Material(
-          color: Colors.transparent,
-          child: Center(
-            child: SizedBox(
-              width: 1280,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Row(
-                        children: [
-                          WidgetGlassBackground(
-                            blur: 2,
-                            backgroundColor: Colors.white10,
-                            borderRadius: BorderRadius.circular(999),
-                            padding: EdgeInsets.zero,
-                            child: SizedBox(
-                              width: 48,
-                              height: 48,
-                              child: Center(
-                                child: Image.asset(assetpng('logo_512')),
-                              ),
-                            ),
-                          ),
-                          const Gap(16),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'imagineeringwithus',
-                                style: w700TextStyle(
-                                    color: Colors.white, fontSize: 24),
-                              ),
-                              const Gap(4),
-                              Text(
-                                'Engineering Your Ideas to Life ',
-                                style: w400TextStyle(
-                                    color: Colors.white.withOpacity(.6)),
-                              ),
-                            ],
-                          ),
-                          const Spacer(),
-                          WidgetRippleButton(
-                            onTap: () {},
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(99),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(99),
-                              ),
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 24, vertical: 12),
-                              child: AnimatedColorBuilder(builder: (color) {
-                                return Text(
-                                  'Contact us',
-                                  style:
-                                      w500TextStyle(fontSize: 16, color: color),
-                                );
-                              }),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    const Spacer(),
-                  ],
-                ),
+        GestureDetector(
+          onLongPress: () {
+            appContext.pushReplacement(AppRoutes.dashboard);
+          },
+          child: Material(
+            color: Colors.transparent,
+            child: Center(
+              child: SizedBox(
+                width: 1280,
               ),
             ),
           ),
